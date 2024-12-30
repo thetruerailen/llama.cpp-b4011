317586d2d9b6435d3302240c2452e7686ea1fbf2
rae <admin@railenbailen.com>
2024-12-30 13:58:04 -0600

    llama compiled

---
+++ common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 0;
+char const *LLAMA_COMMIT = "unknown";
+char const *LLAMA_COMPILER = "Apple clang version 14.0.0 (clang-1400.0.29.202)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin21.6.0";
